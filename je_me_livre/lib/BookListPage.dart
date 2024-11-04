893eb48d8e184a7f0c84a7ff2664bc4c984731da
Damien-Collot <dcollot76@gmail.com>
2024-11-04 09:42:16 +0100

    Feat : search bar

--- je_me_livre/lib/BookListPage.dart
+++ je_me_livre/lib/BookListPage.dart
@@ -12,6 +12,8 @@ class BookListPage extends StatefulWidget {
 class _BookListScreenState extends State<BookListPage> {
   final DatabaseHelper _dbHelper = DatabaseHelper();
   List<Map<String, dynamic>> _books = [];
+  List<Map<String, dynamic>> _filteredBooks = [];
+  String _searchText = '';
 
   @override
   void initState() {
@@ -24,6 +26,8 @@ class _BookListScreenState extends State<BookListPage> {
     final books = await _dbHelper.getBooks();
     setState(() {
       _books = books;
+      _filteredBooks =
+          books; // Initialement, la liste filtrée est identique à la liste complète
     });
   }
 
@@ -84,67 +88,22 @@ class _BookListScreenState extends State<BookListPage> {
     );
   }
 
-  Future<void> _showDeleteConfirmationDialog(int bookId) async {
-    final shouldDelete = await showDialog<bool>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Confirm Deletion'),
-          content: Text('Are you sure you want to delete this book?'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context, false),
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, true),
-              child: Text('Delete'),
-            ),
-          ],
-        );
-      },
-    );
-
-    if (shouldDelete == true) {
-      await _dbHelper.deleteBook(bookId);
-      _loadBooks();
-    }
+  void _filterBooks(String text) {
+    setState(() {
+      _searchText = text;
+      _filteredBooks = _books
+          .where((book) =>
+              book['title'].toLowerCase().contains(text.toLowerCase()))
+          .toList();
+    });
   }
-
-  // Future<void> _deleteAllBooks() async {
-  //   final shouldDelete = await showDialog<bool>(
-  //     context: context,
-  //     builder: (context) {
-  //       return AlertDialog(
-  //         title: Text('Confirm Deletion'),
-  //         content: Text('Are you sure you want to delete all books?'),
-  //         actions: [
-  //           TextButton(
-  //             onPressed: () => Navigator.pop(context, false),
-  //             child: Text('Cancel'),
-  //           ),
-  //           TextButton(
-  //             onPressed: () => Navigator.pop(context, true),
-  //             child: Text('Delete'),
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-
-  //   if (shouldDelete == true) {
-  //     await _dbHelper.deleteAllBooks();
-  //     _loadBooks();
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
-    // Group books by authors
+    // Group books by authors in the filtered list
     final Map<String, List<Map<String, dynamic>>> booksByAuthor = {};
 
-    // Regrouper les livres par auteur
-    for (var book in _books) {
+    for (var book in _filteredBooks) {
       final author = book['author'];
       if (!booksByAuthor.containsKey(author)) {
         booksByAuthor[author] = [];
@@ -157,138 +116,144 @@ class _BookListScreenState extends State<BookListPage> {
         title: Text(
           'Bibliothèque',
           style: TextStyle(
-            color: Colors.black, // Texte noir pour un meilleur contraste
+            color: Colors.black,
           ),
         ),
-        backgroundColor:
-            Color(0xFFD2B48C), // Marron clair en accord avec le bois
-        elevation: 0, // Pas d'ombre sous l'AppBar
-        // actions: [
-        //   IconButton(
-        //     icon: Icon(Icons.delete, color: Colors.black),
-        //     onPressed: _deleteAllBooks,
-        //   ),
-        // ],
+        backgroundColor: Color(0xFFD2B48C),
+        elevation: 0,
       ),
       body: Stack(
         children: [
-          // Effet boisé en arrière-plan (nouvelle image pour le fond)
+          // Effet boisé en arrière-plan
           Positioned.fill(
             child: Image.asset(
-              'assets/wood_background.jpg', // Image de fond boisé
+              'assets/wood_background.jpg',
               fit: BoxFit.cover,
             ),
           ),
-          SingleChildScrollView(
-            child: Column(
-              children: booksByAuthor.entries.map((entry) {
-                final author = entry.key;
-                final books = entry.value;
-
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+          Column(
+            children: [
+              // Champ de recherche
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  onChanged: _filterBooks,
+                  decoration: InputDecoration(
+                    labelText: 'Rechercher un livre',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                  ),
+                ),
+              ),
+              // Utilisation de Expanded pour éviter la zone blanche
+              Expanded(
+                child: SingleChildScrollView(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Noms des auteurs avec un simple texte noir
-                      Padding(
-                        padding: const EdgeInsets.only(left: 16.0),
-                        child: Text(
-                          author,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black, // Couleur du texte des auteurs
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height:
-                            180, // Ajuste la hauteur pour afficher correctement les couvertures des livres
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: books.length,
-                          itemBuilder: (context, index) {
-                            final book = books[index];
-                            final isReserved = book['disponible'] == 0;
+                    children: booksByAuthor.entries.map((entry) {
+                      final author = entry.key;
+                      final books = entry.value;
 
-                            return GestureDetector(
-                              onTap: isReserved
-                                  ? null
-                                  : () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              BookDetailPage(book: book),
-                                        ),
-                                      );
-                                    },
-                              onLongPress: () async {
-                                await _showDeleteConfirmationDialog(book['id']);
-                              },
-                              child: Container(
-                                width:
-                                    120, // Largeur pour chaque carte de livre
-                                margin: EdgeInsets.symmetric(horizontal: 8.0),
-                                decoration: BoxDecoration(
-                                  color: isReserved
-                                      ? Colors.grey[300]
-                                      : Colors.white,
-                                  borderRadius: BorderRadius.circular(8),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black12,
-                                      blurRadius: 4,
-                                      offset: Offset(2, 2),
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    // Image à la place du cadre blanc
-                                    Container(
-                                      height:
-                                          100, // Ajuster la hauteur pour l'image
-                                      child: Image.asset(
-                                        'assets/couverture.png', // Image à utiliser pour le cadre
-                                        fit: BoxFit
-                                            .cover, // S'assurer que l'image couvre bien tout l'espace
-                                      ),
-                                    ),
-                                    Flexible(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          book['title'],
-                                          textAlign: TextAlign.center,
-                                          maxLines:
-                                              2, // Limite à 2 lignes pour éviter le débordement
-                                          overflow: TextOverflow
-                                              .ellipsis, // Ajouter des points si le texte est trop long
-                                          style: TextStyle(
-                                            color: isReserved
-                                                ? Colors.grey
-                                                : Colors.black,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 16.0),
+                              child: Text(
+                                author,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
                                 ),
                               ),
-                            );
-                          },
+                            ),
+                            SizedBox(
+                              height: 180,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: books.length,
+                                itemBuilder: (context, index) {
+                                  final book = books[index];
+                                  final isReserved = book['disponible'] == 0;
+
+                                  return GestureDetector(
+                                    onTap: isReserved
+                                        ? null
+                                        : () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    BookDetailPage(book: book),
+                                              ),
+                                            );
+                                          },
+                                    child: Container(
+                                      width: 120,
+                                      margin:
+                                          EdgeInsets.symmetric(horizontal: 8.0),
+                                      decoration: BoxDecoration(
+                                        color: isReserved
+                                            ? Colors.grey[300]
+                                            : Colors.white,
+                                        borderRadius: BorderRadius.circular(8),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.black12,
+                                            blurRadius: 4,
+                                            offset: Offset(2, 2),
+                                          ),
+                                        ],
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            height: 100,
+                                            child: Image.asset(
+                                              'assets/couverture.png',
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          Flexible(
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                book['title'],
+                                                textAlign: TextAlign.center,
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  color: isReserved
+                                                      ? Colors.grey
+                                                      : Colors.black,
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                      );
+                    }).toList(),
                   ),
-                );
-              }).toList(),
-            ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
--- je_me_livre/lib/ReservationHistoryPage.dart
+++ je_me_livre/lib/ReservationHistoryPage.dart
@@ -10,6 +10,8 @@ class ReservationHistoryPage extends StatefulWidget {
 class _ReservationHistoryPageState extends State<ReservationHistoryPage> {
   final DatabaseHelper _dbHelper = DatabaseHelper();
   List<Map<String, dynamic>> _reservedBooks = [];
+  List<Map<String, dynamic>> _filteredReservedBooks = [];
+  String _searchText = '';
 
   @override
   void initState() {
@@ -21,6 +23,17 @@ class _ReservationHistoryPageState extends State<ReservationHistoryPage> {
     final reservedBooks = await _dbHelper.getReservedBooks();
     setState(() {
       _reservedBooks = reservedBooks;
+      _filteredReservedBooks = reservedBooks; // Initialiser la liste filtrée
+    });
+  }
+
+  void _filterReservedBooks(String text) {
+    setState(() {
+      _searchText = text;
+      _filteredReservedBooks = _reservedBooks
+          .where((book) =>
+              book['title'].toLowerCase().contains(text.toLowerCase()))
+          .toList();
     });
   }
 
@@ -28,28 +41,71 @@ class _ReservationHistoryPageState extends State<ReservationHistoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Reservation'),
+        title: Text(
+          'Réservation',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor:
+            Color(0xFFD2B48C), // Marron clair pour s'accorder au bois
+        elevation: 0,
       ),
-      body: ListView.builder(
-        itemCount: _reservedBooks.length,
-        itemBuilder: (context, index) {
-          final book = _reservedBooks[index];
-          return ListTile(
-            title: Text(book['title']),
-            subtitle: Text('Author: ${book['author']}\nReservation Date: ${book['reservation_start_date']}'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => BookDetailPage(book: book),
+      body: Stack(
+        children: [
+          // Fond bois sur toute la hauteur
+          Positioned.fill(
+            child: Image.asset(
+              'assets/wood_background.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            children: [
+              // Champ de recherche
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  onChanged: _filterReservedBooks,
+                  decoration: InputDecoration(
+                    labelText: 'Rechercher une réservation',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                  ),
                 ),
-              );
-            },
-          );
-        },
+              ),
+              // Liste des réservations filtrées
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _filteredReservedBooks.length,
+                  itemBuilder: (context, index) {
+                    final book = _filteredReservedBooks[index];
+                    return ListTile(
+                      title: Text(
+                        book['title'],
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        'Auteur : ${book['author']}\nDate de réservation : ${book['reservation_start_date']}',
+                        style: TextStyle(color: Colors.black87),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookDetailPage(book: book),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
-
-
